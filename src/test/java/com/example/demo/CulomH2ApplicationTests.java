bb1c43de36a6640c965a8eaf753d59a37cebcdd3
Mohammedaljer <134400465+Mohammedaljer@users.noreply.github.com>
2024-06-09 20:41:55 +0200

    Add files via upload

---
+++ src/test/java/com/example/demo/CulomH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CulomH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
